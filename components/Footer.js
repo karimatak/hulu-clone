b228e6c05be9d08b375072a9b1956fde8cc77254
karimatak <karim.aittalebali@skylark.ma>
2021-04-24 21:08:59 +0000

    adding footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center text-white bg-black">
+      Created By Karim ATAK using Next.js & Tailwind.css • 04/24/2021
+    </footer>
+  );
+};
+
+export default Footer;
