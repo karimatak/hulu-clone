0c2be59fa2f254958162f9b5ba98786fab28c77e
karimatak <karim.aittalebali@skylark.ma>
2021-04-24 15:31:13 +0000

    adding styled header component

---
+++ constants/header-items.js
@@ -0,0 +1,34 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+export const HEADER_ITEMS = [
+  {
+    title: "HOME",
+    icon: HomeIcon,
+  },
+  {
+    title: "TRENDING",
+    icon: LightningBoltIcon,
+  },
+  {
+    title: "VERIFIED",
+    icon: BadgeCheckIcon,
+  },
+  {
+    title: "COLLECTIONS",
+    icon: CollectionIcon,
+  },
+  {
+    title: "SEARCH",
+    icon: SearchIcon,
+  },
+  {
+    title: "ACCOUNT",
+    icon: UserIcon,
+  },
+];
